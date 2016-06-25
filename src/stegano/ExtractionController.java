46550c4c98551aacd9fb72a8ce4be821f1f5c43e
hendro7naga <h.sinaga22@gmail.com>
2016-06-25 22:27:10 +0700

    'Add Extraction Scene and Controller (OK)'

---
+++ src/stegano/ExtractionController.java
@@ -0,0 +1,15 @@
+package stegano;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
+/**
+ * Created by hendro.sinaga on 25-Jun-16.
+ */
+public class ExtractionController {
+    @FXML
+    TextArea textAreaChiper, textAreaOri;
+    @FXML
+    Button btnBrowseStegoImg, btnExtract, btnDecrypt;
+}
